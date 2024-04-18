da7eec7a41b5400e3cbf5b52a4aa4a659313d3e0
marino.villar <marinovillarlara@gmail.com>
2024-04-18 15:27:28 -0400

    Working version

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+      <li>
+        <button>Scan</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+  // ,
+  // methods: {
+  //   scan: function () {
+  //     document.cordova.plugins.barcodeScanner.scan(
+  //       function (result) {
+  //         alert('We got a barcode\n' +
+  //               'Result: ' + result.text + '\n' +
+  //               'Format: ' + result.format + '\n' +
+  //               'Cancelled: ' + result.cancelled)
+  //       },
+  //       function (error) {
+  //         alert('Scanning failed: ' + error)
+  //       },
+  //       {
+  //         preferFrontCamera: true, // iOS and Android
+  //         showFlipCameraButton: true, // iOS and Android
+  //         showTorchButton: true, // iOS and Android
+  //         torchOn: true, // Android, launch with the torch switched on (if available)
+  //         saveHistory: true, // Android, save scan history (default false)
+  //         prompt: 'Place a barcode inside the scan area', // Android
+  //         resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+  //         formats: 'QR_CODE,PDF_417', // default: all but PDF_417 and RSS_EXPANDED
+  //         orientation: 'landscape', // Android only (portrait|landscape), default unset so it rotates with the device
+  //         disableAnimations: true, // iOS
+  //         disableSuccessBeep: false // iOS and Android
+  //       }
+  //     )
+  //   }
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
